7da2d6ab5c2b0af06f6016e85b0c1e11b4bdcc94
bikramkarki7 <karkibikram756@gmail.com>
2026-01-11 18:58:09 +0545

    aa

--- script.js
+++ script.js
@@ -23,30 +23,40 @@
 
 // console.log("D");
 //futsatshal rule 
-const futsalRules = {
-  playersPerTeam: 5,
-  matchDuration: {
-    halves: 2,
-    minutesPerHalf: 20
-  },
-  substitutions: "Unlimited (flying subs)",
-  ball: "Size 4",
-  fouls: {
-    maxBeforePenalty: 5,
-    penaltyAfter: "6th foul = 10m penalty"
-  },
-  goalkeeperRules: {
-    ballHoldSeconds: 4,
-    backPassAllowed: false
-  }
-};
+// const futsalRules = {
+//   playersPerTeam: 5,
+//   matchDuration: {
+//     halves: 2,
+//     minutesPerHalf: 20
+//   },
+//   substitutions: "Unlimited (flying subs)",
+//   ball: "Size 4",
+//   fouls: {
+//     maxBeforePenalty: 5,
+//     penaltyAfter: "6th foul = 10m penalty"
+//   },
+//   goalkeeperRules: {
+//     ballHoldSeconds: 4,
+//     backPassAllowed: false
+//   }
+// };
 
-// Example usage
-console.log(`Players per team: ${futsalRules.playersPerTeam}`);
-console.log(`Match time: ${futsalRules.matchDuration.minutesPerHalf} minutes per half`);
-const wait = ms => new Promise(r => setTimeout(r, ms));
+// // Example usage
+// console.log(`Players per team: ${futsalRules.playersPerTeam}`);
+// console.log(`Match time: ${futsalRules.matchDuration.minutesPerHalf} minutes per half`);
+// const wait = ms => new Promise(r => setTimeout(r, ms));
 
-wait(1000).then(() => console.log("Done"));
+// wait(1000).then(() => console.log("Done"));
+
+let number = Math.floor(Math.random() * 5) + 1;
+let guess = prompt("Guess a number between 1 and 5");
+
+if (guess == number) {
+  alert(" You win!");
+} else {
+  alert(" You lose! The number was " + number);
+}
+
 
 
 
