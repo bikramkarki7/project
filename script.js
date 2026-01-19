a4355da2c713639488339b0e5e3053c694a7c23f
bikramkarki7 <karkibikram756@gmail.com>
2026-01-19 18:53:01 +0545

    2

--- script.js
+++ script.js
@@ -154,7 +154,6 @@ const factorial = n => (n <= 1 ? 1 : n * factorial(n - 1));
 // console.log(fibonacci(7));   // 13
 // console.log(gcd(48, 18));    // 6
 
-// hello.js
 function greet(name) {
   return `Hello, ${name}!`;
 }
